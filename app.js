698221418271365a922081c0eb37c939f6d136a0
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-10-03 12:42:53 +0300

    Change schemas for objects

--- app.js
+++ app.js
@@ -1,28 +1,82 @@
 const mongoose = require('mongoose');
-const { userInfo } = require('os');
 const Schema = mongoose.Schema;
 
-const blogShceme = new Schema({
+const userSchema = new Schema({
+    name: String,
+    lastName: String,
+    nickname: String
+})
+
+const commentSchema = new Schema({
+    text: String,
+    likes: Number,
+    author: userSchema
+})
+
+const postScheme = new Schema({
     title: String,
     body: String,
-    author: String
-});
+    author: userSchema,
+    likes: Number,
+    comments: Array
+}, {versionKey: false});
 
 // подключение
 var connectionString = "mongodb+srv://Zhigadlo:3VQhXcNw1HPh2mJi@amazoncloud.mismmy6.mongodb.net/blogsdb?retryWrites=true&w=majority";
 mongoose.connect(connectionString);
 
-const Blog = mongoose.model("Blog", blogShceme);
+const Post = mongoose.model("Post", postScheme);
+const Comment = mongoose.model("Comment", commentSchema)
+const User = mongoose.model("User", userSchema)
 
-const blog = new Blog({
+const me = new User({
+    name: "Vladislav",
+    lastName: "Zhogol",
+    nickname: "Zhigadlo"
+})
+
+const author1 = new User({
+    name: "Sanya",
+    lastName: "Vishyak",
+    nickname: "Sushnyak"
+})
+
+const author2 = new User({
+    name: "Vanya",
+    lastName: "Andreevec",
+    nickname: "IvanGrozniy"
+})
+
+const comment1 = new Comment({
+    text: "Shit post",
+    likes: 3,
+    author: author1
+})
+
+const comment2 = new Comment({
+    text: "Useful information",
+    likes: 5,
+    author: author2
+})
+
+const post = new Post({
     title: "Test blog title",
     body: "Test text of blog",
-    author: "Vladislav Zhogol"
+    author: me,
+    likes: 10,
+    comments: [comment1, comment2]
 });
 
-blog.save(function(err, createdBlog) {
-    mongoose.disconnect();
-    if (err) return console.log(err);
-  
-    console.log("Сохранен объект", createdBlog);
-  });
+Post.find({name: post.name}, function(err, docs){
+    if(err) return console.log(err)
+
+    if(docs.length > 0){
+        console.log("Database have such blog")
+    }
+    else{
+        post.save(function(err, createdBlog){
+            console.log("Сохранен объект", createdBlog)
+        })
+    }
+    mongoose.disconnect()
+})
